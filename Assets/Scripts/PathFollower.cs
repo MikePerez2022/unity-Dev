932ffe28483073c436ca708de00c8b0b1a882db7
MikePerez2022 <123500465+MikePerez2022@users.noreply.github.com>
2024-01-28 13:55:18 -0700

    made rail shooter scene and added si-fi pack

---
+++ Assets/Scripts/PathFollower.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.Splines;
+
+public class PathFollower : MonoBehaviour
+{
+    [SerializeField] SplineContainer splineContainer;
+    [Range(0,40)] public float speed = 1;
+
+    float tdistance = 1; // distance along spline (0-1)
+
+    //public float speed { get; set; }
+    public float length { get { return splineContainer.CalculateLength(); } }
+    public float distance
+    {
+        get { return tdistance * length; }
+        set { tdistance = value / length; }
+    }
+
+    void Update()
+    {
+        distance += speed;
+        UpdateTransform(math.frac(tdistance));
+    }
+
+    private void UpdateTransform(float t)
+    {
+        Vector3 position = splineContainer.EvaluatePosition(t);
+        Vector3 up = splineContainer.EvaluateUpVector(t);
+        Vector3 forward = Vector3.Normalize(splineContainer.EvaluateTangent(t));
+        Vector3 right = Vector3.Cross(up, forward);
+
+        transform.position = position;
+        transform.rotation = Quaternion.LookRotation(forward, up);
+    }
+}
